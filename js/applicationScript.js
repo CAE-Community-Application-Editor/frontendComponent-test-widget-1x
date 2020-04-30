68acb73c6d2d62770a1d88084c8385d9cfdbf863
CAEUser <mail@rwth>
2020-04-30 11:39:52 +0000

    ApplicationScript edited by Anonymous

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -33,7 +33,7 @@
 var client;
 
 var init = function() {
-  
+  var y = 0;
   var iwcCallback = function(intent) {
     // define your reactions on incoming iwc events here 
     console.log(intent);
